a73c48022d985037ceef17cd24c97f77aa2c7e9f
joleorti <joleorti@espol.edu.ec>
2021-01-29 12:46:38 -0500

    Inicio

--- app/src/main/java/com/amst/evaluacionpractica2/Inicio.java
+++ app/src/main/java/com/amst/evaluacionpractica2/Inicio.java
@@ -3,6 +3,7 @@ package com.amst.evaluacionpractica2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Inicio extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class Inicio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+    }
+
+    public void Buscar(View view) {
     }
 }
